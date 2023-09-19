2944e86cb441c4f475eb183be69ee0e69e71dbae
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-09-19 17:04:36 -0300

    iniciando estilização

--- movie-mais-app/src/App.jsx
+++ movie-mais-app/src/App.jsx
@@ -3,34 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import MyButton from './componentes/button'
+import imagemDeFundo from './img/imagemDeFundo.jpg'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Primeiro contato com React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-
-      <MyButton/>
+      <body style={{
+        backgroundImage: `url(${imagemDeFundo})`
+      }}>
+        <header>
+          <div className='caixa-cabeçalho'></div>
+        </header>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        
+        <MyButton/>
+        
+        
+      </body>
     </>
   )
 }
