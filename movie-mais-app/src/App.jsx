2b10a2cfbb29cb5e7f5f90a0b4d41d89e57f77e0
Daniele Maria <60490226+Danima-dev@users.noreply.github.com>
2023-09-20 20:01:49 -0300

    adicionando url com fetch

--- movie-mais-app/src/App.jsx
+++ movie-mais-app/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
 import imagemDeFundo from './img/imagemDeFundo.jpg';
 import ListaFilmes from './componentes/listaFilmes';
 
@@ -27,56 +27,14 @@ function App() {
       "Type": "movie",
       "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
   },
-  {
-      "Title": "Star Wars: Episode VII - The Force Awakens",
-      "Year": "2015",
-      "imdbID": "tt2488496",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode I - The Phantom Menace",
-      "Year": "1999",
-      "imdbID": "tt0120915",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYTRhNjcwNWQtMGJmMi00NmQyLWE2YzItODVmMTdjNWI0ZDA2XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode III - Revenge of the Sith",
-      "Year": "2005",
-      "imdbID": "tt0121766",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNTc4MTc3NTQ5OF5BMl5BanBnXkFtZTcwOTg0NjI4NA@@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode II - Attack of the Clones",
-      "Year": "2002",
-      "imdbID": "tt0121765",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMDAzM2M0Y2UtZjRmZi00MzVlLTg4MjEtOTE3NzU5ZDVlMTU5XkEyXkFqcGdeQXVyNDUyOTg3Njg@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Rogue One: A Star Wars Story",
-      "Year": "2016",
-      "imdbID": "tt3748528",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode VIII - The Last Jedi",
-      "Year": "2017",
-      "imdbID": "tt2527336",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ1MzcxNjg4N15BMl5BanBnXkFtZTgwNzgwMjY4MzI@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Trek",
-      "Year": "2009",
-      "imdbID": "tt0796366",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BMjE5NDQ5OTE4Ml5BMl5BanBnXkFtZTcwOTE3NDIzMw@@._V1_SX300.jpg"
-  }
+  
 ]);
+  const solicitarFilme = async() => {
+    const url = 'http://www.omdbapi.com/?i=tt3896198&apikey=79bf9410';
+
+    const response = await fetch(url);
+    const responseJson = response.json();
+  }
 
   return  (
     <>
@@ -85,11 +43,14 @@ function App() {
 
       }}>
         
-        <div className='caixa-cabeçalho'> Olá boostrap</div>
+        <div className='caixa-cabeçalho'></div>
+        
 
-       <div className='container-fluid'>
-        <div className='row'></div>
-        <ListaFilmes movies = {movies}/>
+       <div className='container-fluid movie-app'>
+          <div className='row'>
+          <ListaFilmes movies = {movies}/>
+          </div>
+        
        </div>
       
         
